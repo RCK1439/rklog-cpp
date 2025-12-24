d9dcc5a3fd29a2e4cd768f45abea01592fac18f2
Ruan C. Keet <rckeet@gmail.com>
2025-12-24 20:32:58 +0200

    Finished documenting project

--- include/rklog/rklog.hpp
+++ include/rklog/rklog.hpp
@@ -6,7 +6,26 @@
 
 namespace rklog {
 
-ColorLogger& GetColorLogger(std::string_view title = "global") noexcept;
+/**
+ * Gets the global instance of a basic logger
+ *
+ * @param[in] title
+ *      The title to give the basic logger
+ *
+ * @return
+ *      The global basic logger instance
+ */
 BasicLogger& GetBasicLogger(std::string_view title = "global") noexcept;
+
+/**
+ * Gets the global instance of a color logger
+ *
+ * @param[in] title
+ *      The title to give the global logger
+ *
+ * @return
+ *      The global color logger instance
+ */
+ColorLogger& GetColorLogger(std::string_view title = "global") noexcept;
 
 }
