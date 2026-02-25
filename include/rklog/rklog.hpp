05716f12d3f32c81f592396caa89da07c4b35ed3
Ruan C. Keet <rckeet@gmail.com>
2026-02-25 18:13:07 +0200

    Added assertion function

--- include/rklog/rklog.hpp
+++ include/rklog/rklog.hpp
@@ -3,6 +3,8 @@
 #include "Logger/BasicLogger.hpp"
 #include "Logger/ColorLogger.hpp"
 
+#include <exception> // Provides std::terminate()
+
 namespace rklog {
 
 /**
@@ -26,5 +28,29 @@ BasicLogger& GetBasicLogger(std::string_view title = "global") noexcept;
  *      The global color logger instance
  */
 ColorLogger& GetColorLogger(std::string_view title = "global") noexcept;
+
+/**
+ * Asserts that the given expression results to `true`. In the event that the
+ * expression results to `false` a message is logged as "fatal" to the given
+ * logger and the program is terminated via std::terminate()
+ *
+ * @param[in] logger
+ *      The logger to log to if the assertion fails
+ * @param[in] expr
+ *      The expression to assert on
+ * @param[in] fmt
+ *      The format specifier of the log message
+ * @param[in] args
+ *      The arguments for the format specifier
+ */
+template<typename ... Args>
+void Assert(Logger& logger, bool expr, std::format_string<Args...> fmt, Args&& ... args) noexcept
+{
+    if (expr)
+        return;
+
+    logger.Fatal(fmt, std::forward<Args>(args)...);
+    std::terminate();
+}
 
 }
